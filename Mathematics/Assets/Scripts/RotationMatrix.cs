e8cfc778fec834cf23060a375660231c82d91a39
Amin Babadi <amin.babadi@aalto.fi>
2021-07-20 09:47:04 +0300

    Mathematics base project added.

---
+++ Mathematics/Assets/Scripts/RotationMatrix.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationMatrix : MonoBehaviour
+{
+    public float theta = 0;
+    public GameObject rotatedObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
