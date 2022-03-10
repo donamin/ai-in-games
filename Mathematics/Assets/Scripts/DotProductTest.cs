0756931cbbae35c0c612e0bbe4ff6d7ec0b41923
Amin Babadi <amin.babadi@aalto.fi>
2022-03-10 09:43:33 +0200

    pushed codes from the class.

--- Mathematics/Assets/Scripts/DotProductTest.cs
+++ Mathematics/Assets/Scripts/DotProductTest.cs
@@ -16,5 +16,23 @@ public class DotProductTest : MonoBehaviour
     {
         Debug.DrawLine(transform.position, transform.position + transform.forward * 25, Color.blue);
         Debug.DrawLine(transform.position, enemy.transform.position, Color.red);
+
+        Vector3 a = transform.forward;
+        Vector3 b = enemy.transform.position - transform.position;
+
+        float dot_manual = a.x * b.x + a.y * b.y + a.z * b.z;
+        //print(dot_manual);
+        float dot_unity = Vector3.Dot(a, b);
+        //print(dot_unity);
+
+        float alpha = Mathf.Acos(dot_manual / (a.magnitude * b.magnitude)) * Mathf.Rad2Deg;
+        if (Vector3.Dot(b, transform.right) > 0)
+            alpha = -alpha;
+        //print(alpha);
+
+        Vector3 p = b;
+        Vector3 q = transform.right;
+        Vector3 proj = Vector3.Dot(p, q) / q.sqrMagnitude * q;
+        Debug.DrawLine(transform.position, transform.position + proj, Color.green);
     }
 }
